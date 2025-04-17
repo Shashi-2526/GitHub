830397ed6aff79c7a719fdd84c90f7a9e87e0402
hp <hp@LAPTOP-MPMJTA5B>
2025-04-17 11:41:56 +0530

    sync fork is Added

---
+++ GithubProject/src/test/java/syncFork/Sync.java
@@ -0,0 +1,11 @@
+package syncFork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sync {
+@Test
+public void weUpdate() {
+	Reporter.log("sync fork class is updated",true);
+}
+}
